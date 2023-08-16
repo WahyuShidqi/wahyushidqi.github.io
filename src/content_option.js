40ce60add23509d331199c5b9ee5814ba7c6925c
Wahyu Shidqi <wahyushidqi550@gmail.com>
2023-08-16 09:32:33 +0700

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,200 @@
+const logotext = "Wahyu";
+const meta = {
+  title: "Wahyu-Portfolio",
+  description: "I’m Wahyu Shidqi Front-End Dev currenty looking for a job",
+};
+
+const introdata = {
+  title: "I’m Wahyu",
+  animated: {
+    first: "I love coding",
+    second: "I am a Front-End web dev.",
+    third: "I'm available for jobs.",
+  },
+  description:
+    "Hi i'm Wahyu Shidqi. I'm a Front-end developer. Code is really fun for me. I always do my best to learn and improve my coding skill.",
+  your_img_url:
+    "https://images.unsplash.com/photo-1526925539332-aa3b66e35444?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2dyYW1taW5nfGVufDB8MXwwfHx8MA%3D%3D&auto=format&fit=crop&w=1500&q=60",
+};
+
+const dataabout = {
+  title: "About my self",
+  aboutme:
+    "A front-end web developer with a passion for creating visually stunning and user-friendly websites. Passionate about continuous improvement, always stay up-to-date with the latest trends to deliver top-notch results every time",
+};
+const worktimeline = [
+  {
+    jobtitle: "Learn Basic Programming",
+    where: "Coursera",
+    date: "2022",
+  },
+  {
+    jobtitle: "Learn Responsive Web Design",
+    where: "FreeCodeCamp",
+    date: "2022",
+  },
+  {
+    jobtitle: "Learn Responsive Web Design",
+    where: "Codepolitan",
+    date: "2022",
+  },
+  {
+    jobtitle: "Full Stack Web Dev Bootcamp",
+    where: "Harisenin",
+    date: "2023",
+  },
+  {
+    jobtitle: "Participant 0f Binus University’s Cyber Security seminar",
+    where: "Binus University",
+    date: "2022",
+  },
+];
+
+const skills = [
+  {
+    name: "HTML",
+    value: undefined,
+  },
+  {
+    name: "CSS",
+    value: undefined,
+  },
+  {
+    name: "Tailwind",
+    value: undefined,
+  },
+  {
+    name: "Bootstrap",
+    value: undefined,
+  },
+  {
+    name: "Javascript",
+    value: undefined,
+  },
+  {
+    name: "React",
+    value: undefined,
+  },
+  {
+    name: "Node.js",
+    value: undefined,
+  },
+];
+
+const services = [
+  {
+    title: "Front-End development",
+    description: "",
+  },
+  {
+    title: "UI & UX Design",
+    description: "",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "wahyushidqi550@gmail.com",
+  YOUR_FONE: "+62 851 6158 2730",
+  description:
+    "Hi, if you're interested to hire me, you can contact me via my email address.",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_e69ll1c",
+  YOUR_TEMPLATE_ID: "template_3nh380c",
+  YOUR_USER_ID: "VvlOVStC8Pz0LgXNg",
+};
+
+const socialprofils = {
+  github: "https://github.com/WahyuShidqi",
+  facebook: "https://www.instagram.com/wahyu_shidqi/",
+  linkedin: "https://linkedin.com/in/wahyu-shidqi",
+  twitter: "",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
